07fe24c38856fdefafdcbb7b99453883ec1836b8
dan <hi@danbeerman.com>
2016-10-26 20:25:30 -0600

    Progress1
    up to #8

--- main.js
+++ main.js
@@ -15,30 +15,32 @@ problem.
 
 
 
-// When the JS is connected properly, the following code will update the status message. 
+// When the JS is connected properly, the following code will update the status message.
 document.getElementById("q1").innerHTML = "JS Page Connected Properly!";
 document.getElementById("q1").classList.add("status-good");
 
 
 
-// 2. Declare a new variable called 'feelings', 
-//    Then store a string with a word that describes   
-//    how well you feel about this course material. 
-//    Then display that string within the HTML page. 
-
-
-
+// 2. Declare a new variable called 'feelings',
+//    Then store a string with a word that describes
+//    how well you feel about this course material.
+//    Then display that string within the HTML page.
+var feelings = "Fun!";
+// 'Modern' browsers (IE8+, use CSS-style selectors) -- Questions - where are the right places to put script tags in HTML?
+document.getElementById("q2").innerHTML = feelings;
 
 
 
 
 // 3. Declare a new variable called 'name',
 //    store your name within it, and then
-//    create a 'message' variable with a greeting, 
+//    create a 'message' variable with a greeting,
 //    concat the message and name variables
 //    and then display it within the HTML page.
-
-
+var name = "Beerman";
+var message = ", you seem like a great dude, bro.";
+name += message;
+document.getElementById("q3").innerHTML = name;
 
 
 
@@ -47,9 +49,14 @@ document.getElementById("q1").classList.add("status-good");
 // 4. Declare two variables with number values.
 //    Add, subtract, multiply and divide them.
 //    Display the values of each within the HTML page.
-//    Then label these values the best you can. 
-
-
+//    Then label these values the best you can.
+var a = 5;
+var b = 19;
+document.getElementById("q4intro").innerHTML = "a = 5, b = 19. The following are results of basic opperations. "
+document.getElementById("q4a").innerHTML = "Addition: " + (a + b);
+document.getElementById("q4b").innerHTML = "Subtraction: " + (a - b);
+document.getElementById("q4c").innerHTML = "Multiplication: " + (a * b);
+document.getElementById("q4d").innerHTML = "Division: " + (a / b);
 
 
 
@@ -59,7 +66,10 @@ document.getElementById("q1").classList.add("status-good");
 //    Create a third variable that concatenates the two strings
 //    that also places the appropriate space between the strings.
 //    Display the value of the third variable within the HTML page.
-
+var a = "non sum";
+var b = "qualis eram";
+var c = a + " "+ b;
+document.getElementById("q5").innerHTML = c;
 
 
 
@@ -68,25 +78,36 @@ document.getElementById("q1").classList.add("status-good");
 
 // 6. Add, subract, multiply and divide the strings '5' and '3'.
 //    Observe the results and brieftly explain the results within the HTML page.
+var a = '5';
+var b = '3';
+document.getElementById("q6a").innerHTML = "Addition: " + (a + b);
+document.getElementById("q6b").innerHTML = "Subtraction: " + (a - b);
+document.getElementById("q6c").innerHTML = "Multiplication: " + (a * b);
+document.getElementById("q6d").innerHTML = "Division: " + (a / b);
+document.getElementById("q6note").innerHTML = "Type Coersion - javascript is treating the characters as ints and performing the pre-programed math functions and returning the results";
 
 
 
 
 
 
+// 7. When a button is clicked, add content into the ID "q7".
+document.getElementById("callAlert").onclick = function() {q7onClick()};
+function q7onClick() {
+    document.getElementById("q7").innerHTML = "GREAT JOB!";
+}
 
 
-// 7. When a button is clicked, add content into the ID "q7". 
 
 
 
 
-
-
-
-// 8. Collect a number value from the user and when a button is clicked, 
+// 8. Collect a number value from the user and when a button is clicked,
 //    then double user's answer and display that value into the HTML page.
-
+document.getElementById("callDoubleNum").onclick = function() {q8onClick(doubleNum)};
+function q8onClick(doubleNum) {
+    document.getElementById("q8").innerHTML = doubleNum*2;
+}
 
 
 
@@ -95,7 +116,7 @@ document.getElementById("q1").classList.add("status-good");
 
 // 9. Set the variable 'num1' to 5.
 //    Set the variable 'num2' to 10.
-//    Test to see if num1 is greater than num2. 
+//    Test to see if num1 is greater than num2.
 //    If so, output to the HTML page: Yes, the value of num1 is greater than num2
 //    If not, output to the HTML page: No, the value of num1 is not greater than num2
 
@@ -105,12 +126,12 @@ document.getElementById("q1").classList.add("status-good");
 
 
 
-// 10. Set the variable 'score' to a number between 1-100. 
+// 10. Set the variable 'score' to a number between 1-100.
 //    Create the following conditions:
 //    If the score is above 90, output to the HTML page "Great job".
 //    If the score is between 60 and 89, output to the HTML page "Nice work, keep practicing".
 //    If the score is below 59, output to the HTML page "Very bad: study more and retake the test".
-//    Test to make sure each condition is met properly. 
+//    Test to make sure each condition is met properly.
 
 
 
@@ -131,7 +152,7 @@ document.getElementById("q1").classList.add("status-good");
 
 // B. Create the formula to determine the area of a rectangle.
 //     The rectangle is: 2 inches by 3 inches
-//     Display the result in the HTML page. 
+//     Display the result in the HTML page.
 
 
 
@@ -146,7 +167,7 @@ document.getElementById("q1").classList.add("status-good");
 
 
 
-// D. Have the user enter their name into a textfield. When they click a button, find the length (number of characters) of their name and display the result in the HTML page. 
+// D. Have the user enter their name into a textfield. When they click a button, find the length (number of characters) of their name and display the result in the HTML page.
 
 
 
@@ -154,7 +175,7 @@ document.getElementById("q1").classList.add("status-good");
 
 
 
-// E. Create a textfield to request a test score between 0-100 from the user. 
+// E. Create a textfield to request a test score between 0-100 from the user.
 //    If the score is above 90, display in the HTML a message to tell the user "Great job!".
 //    If the score is between 60 and 89, display a message of "Nice work, keep practicing.".
 //    If the score is below 59, display a message of "Very bad: study more and retake the test.".
@@ -166,11 +187,11 @@ document.getElementById("q1").classList.add("status-good");
 
 /* SANDBOX TRACK
 
-Solving all of these problems is a great step in the right direction, 
-but the next step is coming up with your own arrangements to solve 
+Solving all of these problems is a great step in the right direction,
+but the next step is coming up with your own arrangements to solve
 new problems. Practice creating your own problems to solve and their solutions -
-you can even challenge your classmates! 
- 
-Also, consider how you can add/remove CSS styles to create added presentation with the results. 
+you can even challenge your classmates!
+
+Also, consider how you can add/remove CSS styles to create added presentation with the results.
 
 */
